b648244c52009656e78053b49bb2cb45cbbd57d4
Ana-Maria Ilea <62722821+Ana-MariaIlea@users.noreply.github.com>
2020-05-08 10:32:50 +0200

    up

--- GXPEngine_files/GXPEngine/Shop.cs
+++ GXPEngine_files/GXPEngine/Shop.cs
@@ -24,12 +24,16 @@ namespace GXPEngine
                 }
             }
         }
+        void makeShop()
+        {
+
+        }
         void Update()
         {
             foreach (Fish fish in fishList)
             {
-                if (Input.GetMouseButtonDown(button: 0))
-                {
+                //if (Input.GetMouseButtonDown(button: 0))
+                //{
                     if (MyGame.CheckMouseInRectClick(fish.buyToUnlock))
                     {
                         if (_level.currencySystem.money >= fish.coinValue)
@@ -43,7 +47,7 @@ namespace GXPEngine
                             }
                         }
                     }
-                }
+                //}
 
             }
         }
